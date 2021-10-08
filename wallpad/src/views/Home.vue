8bf56ea59340bbfd58e21db32442b99d8129c9b6
jeongseok1 <jeongseok.git@gmail.com>
2021-10-08 11:38:24 +0900

    음성인식

--- wallpad/src/views/Home.vue
+++ wallpad/src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="pa-3">
+    <v-row>
+      <v-col class="pa-2" cols="4" >
+        <div style="border: 10px solid black">
+          <p style="font-size:30px">{{new Date() | moment("MM.DD")}}</p>
+          <p style="font-size:60px;" class="mb-0">{{new Date() | moment("hh")}}</p>
+          <p style="font-size:60px">{{new Date() | moment("mm")}}</p>
+        </div>
+        <div style="border: 10px solid black">
+          날씨<br>날씨<br>날씨<br>날씨<br>날씨<br>날씨<br>날씨<br>날씨<br>날씨<br>날씨<br>날씨<br>날씨<br>날씨<br>날씨<br>날씨
+        </div>
+        
+      </v-col>
+      <v-col class="pa-2" cols="8" style="border: 10px solid black">
+          앱 배치
+          
+      </v-col>
+    </v-row>
+    <div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+
+    components: {
+      
+    },
   }
-}
 </script>
