511ebfcd19681bb5765c722da28e48766f604eb5
jeongseok1 <jeongseok.git@gmail.com>
2021-10-10 22:10:35 +0900

    시간 설정

--- 인터폰/main.py
+++ 인터폰/main.py
@@ -5,7 +5,7 @@ from signal import pause
 
 button = Button(26)
 
-duration = 5.5
+duration = 5
 state = True
 
 def stop():
@@ -13,23 +13,24 @@ def stop():
     state = False
 
 def callback(indata,outdata,frames,time,status):
-    if status:
-        print(status)
+    # if status:
+    #     print(status)
     outdata[:] = indata
 
 def start():
     with sd.Stream(channels=1, callback=callback):  # 녹음데이터가 생길때마다 호출할 callback   
-        print('녹음')
-        while state:
-            sd.sleep(1)
+        print('녹음 시작')
+        # while state:
+        sd.sleep(int(duration*1000))
+        print('녹음 끝')
 
 # try:
 #     with sd.Stream(channels=1, callback=callback):
 
 
     
-    
 
-# button.when_pressed = start
+button.when_pressed = start
 # button.when_released = stop
-# pause()
+pause()
+print('test')
